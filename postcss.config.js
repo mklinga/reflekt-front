dccf9130943acf9eae062498779ed38b3de196f6
Markus Klinga <mail@mklinga.com>
2022-01-07 17:18:21 +0100

    Use tailwindcss instead of postcss for building, fix styles

--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: [
-    require("postcss-import")(),
-    require("tailwindcss")(),
-    ...(process.env.NODE_ENV === 'production' ? [require("autoprefixer")()] : []),
-  ],
-};
--- src/components/Journal/JournalEntry.tsx
+++ src/components/Journal/JournalEntry.tsx
@@ -1,3 +1,4 @@
+import { marked } from 'marked';
 import * as React from 'react';
 import { useParams } from 'react-router-dom';
 import { fetchJournalEntry } from '../../services/journal';
@@ -24,7 +25,28 @@ export default function JournalEntry() {
     );
   }
 
+  const {
+    mood,
+    title,
+    entry,
+    entryDate,
+  } = journalEntry;
+
+  const journalDocument = marked.parse(entry);
+
+  /* eslint-disable react/no-danger */
   return (
-    <span>{JSON.stringify(journalEntry)}</span>
+    <div>
+      <div className="flex py-3 items-center">
+        <span className="text-2xl">{mood}</span>
+        <span className="text-2xl flex-grow pl-3">{title}</span>
+        <span className="text-gray-400 text-sm">{entryDate.toLocaleDateString()}</span>
+      </div>
+      {/*
+        We cannot use tailwind to style the markdown document (not possible to inject style classes)
+        so we will have to use "real" class name and external styling (see journal.css)
+      */}
+      <div className="journal-document" dangerouslySetInnerHTML={{ __html: journalDocument }} />
+    </div>
   );
 }
