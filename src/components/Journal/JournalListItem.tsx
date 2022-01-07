cc07217e137d15d6eeebc2c973d0c328f14c2442
Markus Klinga <mail@mklinga.com>
2022-01-07 11:52:42 +0100

    Use entryDate for the JournalListItem date

--- src/components/Journal/JournalListItem.tsx
+++ src/components/Journal/JournalListItem.tsx
@@ -6,12 +6,12 @@ type Props = {
 }
 
 export default (props: Props) => {
-    const { entry } = props;
+    const { entry: { mood, title, entryDate } } = props;
     return (
         <div className="flex p-3 items-center hover:bg-purple-100 cursor-pointer">
-            <span className="text-2xl">{entry.mood}</span>
-            <span className="text-2xl flex-grow pl-3">{entry.title}</span>
-            <span className="text-gray-400 text-sm">{entry.createdAt.toLocaleString()}</span>
+            <span className="text-2xl">{mood}</span>
+            <span className="text-2xl flex-grow pl-3">{title}</span>
+            <span className="text-gray-400 text-sm">{entryDate.toLocaleDateString()}</span>
         </div>
     )
 }
