fef49dced0ea0becaa81a7f10af9090c17aa2eb2
Markus Klinga <mail@mklinga.com>
2022-03-24 15:41:07 +0200

    Show images in editor, delete on click

--- src/components/Journal/Editor/ModuleDataEditor.tsx
+++ src/components/Journal/Editor/ModuleDataEditor.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { ImageModuleDto, JournalModuleDataType, Modules } from '../../../types/types';
+import { JournalModuleDataType, Modules } from '../../../types/types';
 import ImageModuleEditor from '../Modules/ImageModuleEditor';
 
 type Props = {
@@ -8,24 +8,31 @@ type Props = {
   updateModuleData: React.Dispatch<React.SetStateAction<JournalModuleDataType>>;
 }
 
-function getModuleEditor(moduleName: Modules, moduleData: ImageModuleDto[], entryId: string) {
-  switch (moduleName) {
-    case 'images':
-      return <ImageModuleEditor data={moduleData} entryId={entryId} />;
-    default:
-      return null;
-  }
-}
-
 export default function ModuleDataEditor(props: Props) {
   const { moduleData, updateModuleData, entryId } = props;
   const activeModules: Modules[] = ['images'];
+
+  function getModuleEditor(moduleName: string) {
+    switch (moduleName) {
+      case 'images':
+        return (
+          <ImageModuleEditor
+            data={moduleData.images}
+            entryId={entryId}
+            updateModuleData={updateModuleData}
+          />
+        );
+
+      default:
+        return null;
+    }
+  }
 
   return (
     <div>
       {activeModules.map((module: Modules) => (
         <div key={module}>
-          {getModuleEditor(module, moduleData[module], entryId)}
+          {getModuleEditor(module)}
         </div>
       ))}
     </div>
--- src/components/Journal/Modules/ImageModuleEditor.tsx
+++ src/components/Journal/Modules/ImageModuleEditor.tsx
@@ -1,27 +1,74 @@
 import * as React from 'react';
-import { ImageModuleDto } from '../../../types/types';
+import { ImageModuleDto, JournalModuleDataType } from '../../../types/types';
 
 type Props = {
   data: ImageModuleDto[];
   entryId: string;
+  updateModuleData: React.Dispatch<React.SetStateAction<JournalModuleDataType>>;
 }
 
-function getUploadedImageString(data: ImageModuleDto[]) {
-  const imageNames = data.map((image) => image.name).join(',');
-  return `Uploaded images: ${imageNames}`;
+function removeImage(
+  imageId: string,
+  updateModuleData: React.Dispatch<React.SetStateAction<JournalModuleDataType>>,
+) {
+  return async function onClick() {
+    const response = await fetch(`/api/images/${imageId}`, { method: 'DELETE' });
+    if (response.status !== 200) {
+      // TODO: Do something else //
+      /* eslint-disable no-console */
+      console.error('DELETING image failed 🤷‍♀️');
+      console.log(response);
+      /* eslint-enable no-console */
+      return;
+    }
+
+    updateModuleData(
+      (data) => ({ ...data, images: data.images.filter((image) => image.id !== imageId) }),
+    );
+  };
 }
 
-export default function ImageModuleEditor(props: Props) {
-  const { data, entryId } = props;
+function imageBar(
+  data: ImageModuleDto[],
+  updateModuleData: React.Dispatch<React.SetStateAction<JournalModuleDataType>>,
+) {
+  return (
+    <p>
+      {data.map((image) => (
+        <button key={image.id} type="button" onClick={removeImage(image.id, updateModuleData)}>
+          <img className="h-40 mr-1" src={`/api/images/${image.id}`} alt={image.name} />
+        </button>
+      ))}
+    </p>
+  );
+}
 
-  function onFileSelection(event: React.ChangeEvent<HTMLInputElement>) {
+function getOnChangeFunction(
+  entryId: string,
+  updateModuleData: React.Dispatch<React.SetStateAction<JournalModuleDataType>>,
+) {
+  return async function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     /* TODO: move all this to some service */
     const formData = new FormData();
     formData.append('file', event.target.files[0]);
     formData.append('journalEntry', entryId);
 
-    fetch('/api/images', { method: 'POST', body: formData });
-  }
+    const response = await fetch('/api/images', { method: 'POST', body: formData });
+    if (response.status !== 200) {
+      // TODO: Do something else //
+      /* eslint-disable no-console */
+      console.error(response);
+      /* eslint-enable no-console */
+      return;
+    }
+
+    const savedImage = await response.json() as ImageModuleDto;
+    updateModuleData((data) => ({ ...data, images: [...data.images, savedImage] }));
+  };
+}
+
+export default function ImageModuleEditor(props: Props) {
+  const { data, entryId, updateModuleData } = props;
 
   if (!entryId) {
     return null;
@@ -31,11 +78,16 @@ export default function ImageModuleEditor(props: Props) {
     <div>
       <div className="flex justify-between items-center">
         <span>
-          {data && data.length > 0 ? getUploadedImageString(data) : 'No uploaded images'}
+          {data && data.length > 0 ? imageBar(data, updateModuleData) : 'No uploaded images'}
         </span>
         <label htmlFor="imageUploadInput">
           Upload new image
-          <input className="hidden" id="imageUploadInput" type="file" onChange={onFileSelection} />
+          <input
+            className="hidden"
+            id="imageUploadInput"
+            type="file"
+            onChange={getOnChangeFunction(entryId, updateModuleData)}
+          />
         </label>
       </div>
     </div>
