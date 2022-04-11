1575588a5003308eadd73cbc035a1d36ddbbfd9d
Markus Klinga <mail@mklinga.com>
2022-04-11 09:52:44 +0300

    Initial tagselector functionality

--- src/components/Common/Tag.tsx
+++ src/components/Common/Tag.tsx
@@ -3,11 +3,20 @@ import { TagModuleDto } from '../../types/types';
 
 type Props = {
   tag: TagModuleDto;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
 export default function Tag(props: Props) {
-  const { tag: { name, color, id } } = props;
+  const { tag: { name, color, id }, onClick } = props;
   const style = { color, border: `1px solid ${color}` };
 
-  return <span className="rounded-md px-1.5 py-0.5 text-xs mr-3" style={style}>{name}</span>;
+  return (
+    <button onClick={onClick} type="button" className="rounded-md px-1.5 py-0.5 text-xs mr-3" style={style}>
+      {name}
+    </button>
+  );
 }
+
+Tag.defaultProps = {
+  onClick: undefined,
+};
--- src/components/Journal/Modules/TagModuleEditor.tsx
+++ src/components/Journal/Modules/TagModuleEditor.tsx
@@ -9,22 +9,56 @@ type Props = {
   updateModuleData: React.Dispatch<React.SetStateAction<JournalModuleDataType>>;
 }
 
-function AvailableTags({ tags }: { tags: TagModuleDto[] }) {
+type TagSelectorProps = {
+  tags: TagModuleDto[],
+  visible: boolean,
+  setDirty: React.Dispatch<React.SetStateAction<boolean>>
+  toggleTagFn: (tag: TagModuleDto) => void,
+}
+
+function TagSelector({
+  tags, visible, setDirty, toggleTagFn,
+}: TagSelectorProps) {
   const className = classes([
-    'hidden', 'peer-focus:block', 'shadow-2xl', 'shadow-[7px_7px_pink]',
-    'border', 'border-dashed', 'border-black', 'bg-white', 'p-4',
+    'absolute', 'w-full', visible ? 'visible' : 'hidden', 'border', 'border-dashed', 'border-black', 'bg-white', 'p-4',
   ]);
+
+  const handleClick = (tag: TagModuleDto) => (e: React.MouseEvent<HTMLButtonElement>) => {
+    setDirty(true);
+    toggleTagFn(tag);
+    e.preventDefault();
+  };
 
   return (
     <div className={className}>
-      {tags.map((tag) => <Tag key={tag.id} tag={tag} />)}
+      {tags.map((tag) => <Tag key={tag.id} tag={tag} onClick={handleClick(tag)} />)}
     </div>
   );
+}
+
+function toggleTag(
+  updateModuleData: React.Dispatch<React.SetStateAction<JournalModuleDataType>>,
+) {
+  return function toggle(tag: TagModuleDto) {
+    updateModuleData((data: JournalModuleDataType) => {
+      const hasTag = data.tags.find((existingTag) => existingTag.id === tag.id);
+      return {
+        ...data,
+        tags: hasTag ? data.tags.filter((x) => x.id !== tag.id) : data.tags.concat(tag),
+      };
+    });
+  };
 }
 
 export default function TagModuleEditor(props: Props) {
   const { data, entryId, updateModuleData } = props;
   const [allTags, setAllTags] = React.useState([]);
+  const [tagSelectorVisible, setTagSelectorVisible] = React.useState(false);
+  const [dirty, setDirty] = React.useState(false);
+
+  const hasTags = Array.isArray(data) && data.length > 0;
+  const tagLine = hasTags ? data.map((tag) => <Tag key={tag.id} tag={tag} />) : 'No tags';
+  const tagLineClass = classes(['w-full', 'mr-6', 'text-sm', 'text-left', hasTags ? null : 'text-gray-400']);
 
   React.useEffect(() => {
     fetch('/api/tags/').then((response) => response.json()).then((allTagData: TagModuleDto[]) => {
@@ -35,20 +69,29 @@ export default function TagModuleEditor(props: Props) {
   if (!entryId) {
     return null;
   }
+  const toggleTagSelector = () => setTagSelectorVisible(!tagSelectorVisible);
 
   return (
     <div>
       <div className="flex justify-between items-center">
         <div className="relative w-full">
-          <input
-            className="w-full mr-6 peer"
-            placeholder="No tags"
-            type="text"
-            defaultValue={data.map((tag) => tag.name).join(', ')}
+          <div
+            role="button"
+            onKeyPress={toggleTagSelector}
+            tabIndex={0}
+            className={tagLineClass}
+            onClick={toggleTagSelector}
+          >
+            {tagLine}
+          </div>
+          <TagSelector
+            tags={allTags}
+            visible={tagSelectorVisible}
+            setDirty={setDirty}
+            toggleTagFn={toggleTag(updateModuleData)}
           />
-          <AvailableTags tags={allTags} />
         </div>
-        <button type="button">Update</button>
+        <button type="button" className={dirty ? 'text-pink-500' : ''}>Update</button>
       </div>
     </div>
   );
