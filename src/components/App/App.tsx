e567e605e42aa0e74b8adfa3f339730731fdf029
Markus Klinga <mail@mklinga.com>
2022-06-03 09:37:37 +0300

    Add favicon, make dev header pop out

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,18 @@ import * as React from 'react';
 import { Outlet } from 'react-router';
 import NavigationLinks from './NavigationLinks';
 
+function isLocalHost() {
+  return window.location.hostname.match(/localhost/);
+}
+
 export default function App() {
+  const header = isLocalHost()
+    ? <h1 className="text-xl font-medium text-white bg-cyan-600 border-b border-black p-3">Reflekt - DEVELOPMENT</h1>
+    : <h1 className="text-xl font-medium text-black border-b border-black py-3">Reflekt</h1>;
   return (
     <div>
       <header className="mb-3">
-        <h1 className="text-xl font-medium text-black border-b border-black py-3">Reflekt</h1>
+        {header}
         <NavigationLinks />
       </header>
       <Outlet />
