d9b15d919ac80a3033b071c9e14ae63df3783112
Markus Klinga <mail@mklinga.com>
2022-06-03 09:45:16 +0300

    Add podman commands to build

--- build.sh
+++ build.sh
@@ -9,4 +9,6 @@ npm run build:css
 npm run build:js
 npm run update-journal-css
 
-podman build -t reflekt-frontend:1.0 .
+podman build -t markusklinga/reflekt-front:0.1 .
+
+podman push markusklinga/reflekt-front:0.1
