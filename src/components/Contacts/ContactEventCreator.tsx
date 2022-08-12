8bd0abaf1ed0e64983e3ac5dc53324e895f7966b
Markus Klinga <mail@mklinga.com>
2022-08-12 18:45:35 +0300

    Add default participants to new contactevents

--- src/components/Contacts/ContactEventCreator.tsx
+++ src/components/Contacts/ContactEventCreator.tsx
@@ -1,31 +1,55 @@
 import * as React from 'react';
 import { useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router';
+import useQuery from '../../hooks/useQuery';
 import { addNewContactEvent } from '../../services/contactEvents';
 import { selectContacts } from '../../store/contacts/contactSelector';
+import { selectUserContactId } from '../../store/user/userSelectors';
 import { ContactEvent } from '../../types/contactEventTypes';
+import { Contact } from '../../types/contactTypes';
 import { getISODateString } from '../../utils/date';
 import ActionButton from '../Common/ActionButton';
 import H1 from '../Common/headers/H1';
 import TextArea from '../Common/TextArea';
 import TextInput from '../Common/TextInput';
 
-const initialEventDraft: ContactEvent = {
+const convertParticipantIdsToParticipants = (contacts: Contact[], ids: string[]) => ids
+  .map((id) => contacts.find((contact) => contact.id === id))
+  .filter((x) => x);
+
+const initialEventDraft = (contacts: Contact[], participantIds: string[]): ContactEvent => ({
   id: '00000000-0000-0000-0000-000000000000',
   title: '',
   description: '',
   eventDate: getISODateString(new Date()),
   eventType: 'IN_PERSON',
-  participants: [],
-};
+  participants: convertParticipantIdsToParticipants(contacts, participantIds),
+});
 
 export default function ContactEventCreator() {
+  const navigate = useNavigate();
+  const params = useQuery();
+  const withParticipantId = params.get('withParticipant');
+  const userContactId = useSelector(selectUserContactId);
+
+  const initialContacts = React.useMemo(() => {
+    const initialContactIds = [];
+    initialContactIds.push(userContactId);
+
+    if (withParticipantId) {
+      initialContactIds.push(withParticipantId);
+    }
+
+    return initialContactIds;
+  }, [userContactId, withParticipantId]);
+
   const contacts = useSelector(selectContacts);
-  const [draft, setDraft] = React.useState(initialEventDraft);
+  const [draft, setDraft] = React.useState(initialEventDraft(contacts, initialContacts));
 
   const createEvent = async () => {
     /* TODO: validate before sending */
     await addNewContactEvent(draft);
-    /* TODO: do something after creation (navigate to contact page etc.) */
+    navigate('/contacts');
   };
 
   const changeInputValue = (field: string) => (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -44,9 +68,7 @@ export default function ContactEventCreator() {
     }
 
     const participantIds = e.target.value.split(',').map((item) => item.trim());
-    const participants = participantIds
-      .map((id) => contacts.find((contact) => contact.id === id))
-      .filter((x) => x);
+    const participants = convertParticipantIdsToParticipants(contacts, participantIds);
 
     setDraft((old) => ({ ...old, participants }));
   };
@@ -93,7 +115,7 @@ export default function ContactEventCreator() {
           <label className="flex">
             <span className="mr-3 w-48 text-right inline-block">Participants</span>
             <TextInput
-              defaultValue={draft.participants.join(', ')}
+              defaultValue={initialContacts.join(', ')}
               className="grow"
               onChange={changeParticipants}
             />
